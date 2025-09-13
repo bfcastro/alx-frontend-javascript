9ac717a65ea268fa070f0e196a4799ac04def4ec
bfcastro <baffourcastro@gmail.com>
2025-09-13 21:44:22 +0000

    main

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,55 @@
+//Interface
+interface DirectorInterface {
+workFromHome(): string;
+getCoffeeBreak(): string;
+workDirectoryTasks(): string;
+}
+
+interface TeacherInterface {
+workFromHome(): string;
+getCoffeeBreak(): string;
+workTeacherTasks(): string;
+}
+
+//Director class
+class Director implements DirectorInterface {
+workFromHome(): string {
+return "Working from home";
+}
+
+getCoffeeBreak(): string {
+return "Getting a coffee break";
+}
+
+workDirectorTasks(): string {
+return "Getting to director tasks"
+}
+}
+
+//Teacher class
+class Teacher implements TeacherInterface {
+workFromHome(): string {
+return "Cannot work from home";
+}
+
+getCoffeeBreak(): string {
+return "Cannot have a break";
+}
+
+workTeacherTasks(): string {
+return "Getting to work";
+}
+}
+
+//Employment function
+function createEmployee(salary: number | string): Director | Teacher {
+if (typeof salary === "number" && salary < 500) {
+return new Teacher();
+}
+return new Director();
+}
+
+//Examples
+console.log(createEmployee(200)); // Teacher
+console.log(createEmployee(1000)); // Director
+console.log(createEmployee('$500')); // Director
