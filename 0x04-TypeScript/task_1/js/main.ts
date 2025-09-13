a2787cd160ea4e1f4b4b1b440bd6986b0713723f
bfcastro <baffourcastro@gmail.com>
2025-09-13 20:06:21 +0000

    main

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -13,7 +13,27 @@ interface Director extends Teacher {
   numberOfReports: number;
 }
 
-// Usage example
+// Function interface
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+// Function implementation
+const printTeacher: printTeacherFunction = (firstName: string, lastName: string): string => {
+  return `${firstName.charAt(0)}. ${lastName}`;
+};
+
+// Example Teacher
+const teacher3: Teacher = {
+  firstName: 'John',
+  lastName: 'Doe',
+  fullTimeEmployee: false,
+  location: 'London',
+  contract: false,
+};
+console.log(teacher3);
+
+// Example Director
 const director1: Director = {
   firstName: 'John',
   lastName: 'Doe',
@@ -21,5 +41,8 @@ const director1: Director = {
   fullTimeEmployee: true,
   numberOfReports: 17,
 };
-
 console.log(director1);
+
+// Example printTeacher usage
+console.log(printTeacher("John", "Doe"));
+console.log(printTeacher("Mary", "Poppins"));
