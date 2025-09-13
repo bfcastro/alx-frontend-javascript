ce035acd4890b92bcc214b56ac46dd7dc8c2a703
bfcastro <baffourcastro@gmail.com>
2025-09-13 19:06:11 +0000

    main

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,20 +1,25 @@
-// Define Teacher interface
+// Teacher interface
 interface Teacher {
   readonly firstName: string;
   readonly lastName: string;
   fullTimeEmployee: boolean;
   yearsOfExperience?: number;
   location: string;
-  [key: string]: any;
+  [key: string]: any; // allow additional properties
+}
+
+// Directors interface extending Teacher
+interface Directors extends Teacher {
+  numberOfReports: number;
 }
 
 // Usage example
-const teacher3: Teacher = {
+const director1: Directors = {
   firstName: 'John',
   lastName: 'Doe',
-  fullTimeEmployee: false,
   location: 'London',
-  contract: false,
+  fullTimeEmployee: true,
+  numberOfReports: 17,
 };
 
-console.log(teacher3);
+console.log(director1);
