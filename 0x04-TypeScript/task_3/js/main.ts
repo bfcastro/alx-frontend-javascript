74e3c72d7e643dd56409a5c873b1df375f203307
bfcastro <baffourcastro@gmail.com>
2025-09-14 00:38:35 +0000

    main

---
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -0,0 +1,18 @@
+import { RowID, RowElement } from "./interface";
+import * as CRUD from "./crud";
+
+// Create a row object
+const row: RowElement = {
+  firstName: "Guillaume",
+  lastName: "Salva",
+};
+
+// Insert row
+const newRowID: RowID = CRUD.insertRow(row);
+
+// Update row
+const updatedRow: RowElement = { ...row, age: 23 };
+CRUD.updateRow(newRowID, updatedRow);
+
+// Delete row
+CRUD.deleteRow(newRowID);
